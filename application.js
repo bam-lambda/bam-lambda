075fc80f14addaba0a2463b4530e1376882c447f
jasonoverby <disguisekit@gmail.com>
2018-12-24 08:05:46 -0800

    add nav

--- application.js
+++ application.js
@@ -1,1 +1,26 @@
-console.log('hi');
+/* global document */
+document.addEventListener('DOMContentLoaded', () => {
+  const bamLogo = document.querySelector('#bam-logo');
+  const nav = document.querySelector('nav');
+  const main = document.querySelector('main');
+  const closeNav = document.querySelector('#close-nav');
+  const homeLink = document.querySelector('#home-link');
+  const caseStudyLink = document.querySelector('#case-study-link');
+  // const ourTeam = document.querySelector('#our-team');
+
+  const showNav = () => {
+    nav.style.display = 'block';
+    main.style.display = 'none';
+  };
+
+  const hideNav = () => {
+    nav.style.display = 'none';
+    main.style.display = 'block';
+  };
+
+  bamLogo.addEventListener('click', showNav);
+  closeNav.addEventListener('click', hideNav);
+  homeLink.addEventListener('click', hideNav);
+  caseStudyLink.addEventListener('click', hideNav);
+  // ourTeamLink.addEventListener('click', hideNav);
+});
